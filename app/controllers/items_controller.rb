5b1455a68185c67b9263af8ac21a7cb4c330c45a
kawaji-ryuto <ryuto06040604@gmail.com>
2019-02-09 16:37:08 +0900

    登録機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,21 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:nsme, :price, :seller, :description, :email, :image_url)
+  end
+  
 end
